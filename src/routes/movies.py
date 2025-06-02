8bb38764008c6d41374388d912166b369e739f13
Claymore-AI <dimalishchuk2003@gmail.com>
2025-06-02 19:24:24 +0300

    add all endpoint

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,13 +1,34 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select, func
-from sqlalchemy.exc import IntegrityError
+from fastapi import APIRouter, Depends, HTTPException, Query, Path
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload
+from sqlalchemy.future import select
+from sqlalchemy.exc import IntegrityError
 
 from database import get_db, MovieModel
 from database.models import CountryModel, GenreModel, ActorModel, LanguageModel
 
-
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies")
+async def list_movies():
+    pass
+
+
+@router.post("/movies")
+async def create_movie():
+    pass
+
+
+@router.get("/movies/{movie_id}")
+async def get_movie_details():
+    pass
+
+
+@router.delete("/movies/{movie_id}")
+async def delete_movie():
+    pass
+
+
+@router.put("/movies/{movie_id}")
+async def update_movie():
+    pass
